1a443a1f290518be8ddf65c096607b1af8975c17
M. Tim Jones <mtj@mtjones.com>
2024-07-27 13:58:07 -0600

    Added env var setup script.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run this before starting the server (python3 chatmail.py)
+export pop3_server=<Your POP3 Server FQDN>
+export smtp_server=<Your SMTP Server FQDN>
+export username=<Your target email address>
+export password=<Your target email password>
